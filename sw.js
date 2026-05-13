888774851dd6f85e1040232c4869613b97d59f97
ChristophTAA <ctaa@live.no>
2026-05-13 12:24:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,20 @@
-const CACHE = 'chriscalendar-v2';
-const FILES = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+const CACHE = 'chriscalendar-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k=>caches.delete(k)))));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request).then(response => {
+      const clone = response.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return response;
+    }).catch(() => caches.match(e.request))
+  );
 });
